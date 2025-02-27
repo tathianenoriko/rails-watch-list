fbea4c57f6d36d343559d95981e62947fb6f6c85
Tathiane Noriko <tathianenoriko@gmail.com>
2025-02-27 11:40:42 -0300

    set up

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-
+import "@popperjs/core"
+import "bootstrap"
 const application = Application.start()
 
 // Configure Stimulus development experience
